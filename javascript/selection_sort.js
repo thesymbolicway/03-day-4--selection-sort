8737cc2b6c71bfbdc71f95ecd3d84f40633fd810
Daniel <daniel.arisa@yahoo.com>
2023-01-22 22:52:03 -0600

    first commit

---
+++ javascript/selection_sort.js
@@ -0,0 +1,38 @@
+function selectionSort(arr) {
+  let startTime = Date.now();
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
+}
+
+
+if (require.main === module) {
+  // add your own tests in here
+  console.log("Expecting: [-1, 2, 3, 5]");
+  console.log("=>", selectionSort([3, -1, 5, 2]));
+
+  console.log("");
+
+  // BENCHMARK HERE, and print the average runtime
+  const longInput = [];
+
+  for (let i = 0; i < 100; ++i) {
+    longInput.push(Math.random());
+  }
+}
+
+module.exports = selectionSort;
+
+// Please add your pseudocode to this file
+// And a written explanation of your solution
